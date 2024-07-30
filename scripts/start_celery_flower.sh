b3154d2d8cd14d6605178e5d398b0bf716bb1238
Rewaster <bergdanramir@gmail.com>
2024-07-30 17:41:43 +0700

    fix: fixed Flower broker interaction

--- scripts/start_celery_flower.sh
+++ scripts/start_celery_flower.sh
@@ -3,4 +3,4 @@
 set -o errexit
 set -o nounset
 
-celery -A fastapi_auth.tasks.celery_conf flower --loglevel=info
+celery -A fastapi_auth.tasks.celery_conf flower --loglevel=info --broker-api=http://"${MQ_USER}":"${MQ_PASSWORD}"@"${MQ_HOST}":15672/api/vhost
